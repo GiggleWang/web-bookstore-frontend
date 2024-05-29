00f7eb01dccc1468f93087838e67017606ea20d6
GiggleWang <2386181363@qq.com>
2024-05-29 22:06:13 +0800

    改变memberPage，分页从后端实现

--- src/pages/MemberPage.jsx
+++ src/pages/MemberPage.jsx
@@ -2,15 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { Table, Button, message } from 'antd';
 import api from "../service/axios";
 
-const fetchUsers = async () => {
+const fetchUsers = async (page, pageSize) => {
     try {
-        const response = await api.get(`${process.env.REACT_APP_API_URL}/api/admin/users`);
+        const response = await api.get(`${process.env.REACT_APP_API_URL}/api/admin/users?page=${page}&size=${pageSize}`);
         return response.data;
     } catch (error) {
         console.error('Error fetching users:', error);
         throw error;
     }
 };
+
 
 const updateUserStatus = async (userId, action) => {
     try {
@@ -25,12 +26,16 @@ const updateUserStatus = async (userId, action) => {
 const MemberPage = () => {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const data = await fetchUsers();
-                setUsers(data);
+                const data = await fetchUsers(currentPage - 1, pageSize);
+                setUsers(data.content);
+                setTotal(data.totalElements);
                 setLoading(false);
             } catch (error) {
                 console.error(error);
@@ -39,8 +44,12 @@ const MemberPage = () => {
         };
 
         fetchData();
-    }, []);
+    }, [currentPage, pageSize]);
 
+    const handleTableChange = (pagination) => {
+        setCurrentPage(pagination.current);
+        setPageSize(pagination.pageSize);
+    };
     const handleDisableUser = async (userId) => {
         await updateUserStatus(userId, 'disable');
         setUsers(users.map(user => (user.id === userId ? { ...user, disabled: true } : user)));
@@ -86,7 +95,18 @@ const MemberPage = () => {
     return (
         <div>
             <h1>用户管理</h1>
-            <Table columns={columns} dataSource={users} rowKey="id" loading={loading} />
+            <Table
+                columns={columns}
+                dataSource={users}
+                rowKey="id"
+                loading={loading}
+                pagination={{
+                    current: currentPage,
+                    pageSize: pageSize,
+                    total: total,
+                    onChange: handleTableChange
+                }}
+            />
         </div>
     );
 };
